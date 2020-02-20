5a464aff0b106aee6c524daf449ad5fbf23c8664
Dwiky <dwikboss99@gmail.com>
2020-02-20 09:50:13 +0100

    Added empty municipality dropdown

---
+++ resources/js/components/UI/Dropdown.vue
@@ -0,0 +1,14 @@
+<template>
+    <select id="municipalities">
+        <option value="municipality">municipality</option>
+    </select>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
