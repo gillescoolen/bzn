690a649cf38fb39841015c7cc7a87d81fb28a678
Dwiky <dwikboss99@gmail.com>
2020-02-21 16:23:54 +0100

    Added question table model, seeders and factory

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class QuestionTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(QuestionTableSeeder::class);
-        $this->call(MunicipalityTableSeeder::class);
+        factory(App\Question::class, 10)->create();
     }
 }
