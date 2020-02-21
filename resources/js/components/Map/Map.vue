54f741bb74e6f57da6a10f199dd815db32b703ce
Gilles Coolen <gilleswhc@gmail.com>
2020-02-21 16:32:00 +0100

    Add Leaflet map to home component

---
+++ resources/js/components/Map/Map.vue
@@ -0,0 +1,37 @@
+<template>
+    <l-map style="height: 100%; width: 100%;" :zoom="zoom" :center="center" :options="options">
+        <l-tile-layer :url="url"></l-tile-layer>
+    </l-map>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from 'vue2-leaflet';
+export default {
+    components: {
+        LMap,
+        LTileLayer,
+    },
+
+    props: {
+        latitude: {
+            default: 5.322876
+        },
+        longitude: {
+            default: 51.685541
+        },
+    },
+
+    data() {
+        return {
+            zoom: 10,
+            bounds: null,
+            options: {
+                zoomControl: false
+            },
+            center: [this.longitude, this.latitude],
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        };
+    },
+}
+</script>
