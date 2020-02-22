e2f2ccc0b205a4f01fe6c3124bb12932e08c15e1
Gilles Coolen <gilleswhc@gmail.com>
2020-02-22 17:24:03 +0100

    Remove unused user auth middleware

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/municipalities', function(){
     $municipality = ['name' => 'Horn'];
     return $municipality;
