f5bc4d9e2ea32cfd239bf4b620e6d8ae37b88537
Gilles Coolen <gilleswhc@gmail.com>
2020-04-03 18:18:55 +0200

    Remove unnecessary data from seeder and add admin account

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,20 +15,22 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'id' => 1,
                 'name' => 'Tijs Verbeek',
                 'email' => 'tijsverbeek@gmail.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password = password
-                'remember_token' => Str::random(10),
             ],
             [
-                'id' => 2,
                 'name' => 'Jeroen Raams',
                 'email' => 'jraams@avans.nl',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password = password
-                'remember_token' => Str::random(10),
+            ],
+            [
+                'name' => 'Ad Min',
+                'email' => 'admin@bzn.nl',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password = password
             ]
         ]);
     }
