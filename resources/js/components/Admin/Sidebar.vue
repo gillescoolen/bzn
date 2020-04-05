e99415526d732f78c68d9e31c27236e41b952297
Gilles Coolen <gilleswhc@gmail.com>
2020-04-05 13:03:59 +0200

    Rename 'Users' to 'Gebruikers'

--- resources/js/components/Admin/Sidebar.vue
+++ resources/js/components/Admin/Sidebar.vue
@@ -18,7 +18,7 @@ export default {
         return {
             items: [
                 {
-                    name: "Users",
+                    name: "Gebruikers",
                     path: "/admin/users"
                 }
             ]
