151057a80bfffc4d8dc4965c9b6e61454ec68f4b
Gilles Coolen <gilleswhc@gmail.com>
2020-04-06 09:03:11 +0200

    Temporarily remove user dropdown fetch

--- resources/js/components/UI/UserDropdown.vue
+++ resources/js/components/UI/UserDropdown.vue
@@ -1,58 +1,13 @@
 <template>
-    <div class="wrapper" v-if="username">
-        <span v-if="show" @click="show = false">Sluiten</span>
-        <span v-else @click="show = true">Welkom {{ user.name }} !</span>
-    </div>
-    <div class="wrapper" v-else>
+    <div class="wrapper">
         <a href="/login/">Inloggen</a>
     </div>
 </template>
 
-<script>
-
-    export default {
-        name: "UserDropdown",
-        components: {
-        },
-
-        data() {
-            return {
-                show: false,
-                user: null,
-                username: null
-            }
-        },
-
-        async mounted() {
-            this.user = await this.fetchAuthUser();
-            this.username = await this.fetchAuthUsername();
-        },
-
-        methods: {
-            async fetchAuthUser() {
-                try {
-                    const res = await fetch('api/user');
-                    return await res.json()
-                } catch (error) {
-                    console.error('Error fetching user: ', error);
-                }
-            },
-            async fetchAuthUsername() {
-                try {
-                    const res = await fetch('api/username');
-                    return await res.json()
-                } catch (error) {
-                    console.error('Error fetching username: ', error);
-                }
-            }
-        },
-    }
-</script>
-
 <style lang="scss" scoped>
 
 .wrapper {
-    width:200px!important;
+    width: 200px;
     text-align: right;
     font-weight: bold;
     padding:20px;
