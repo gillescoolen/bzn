06c861d3e53277354d90dca9f5110a506e1eae7e
Gilles Coolen <gilleswhc@gmail.com>
2020-04-07 16:55:03 +0200

    Add role to user call

--- app/Role.php
+++ app/Role.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    protected $table = 'role';
+    protected $table = 'roles';
     protected $primaryKey = 'role';
     protected $keyType = 'string';
     public $incrementing = false;
