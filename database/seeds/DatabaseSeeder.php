c5acb4032b5a1e46f0c2737e2b43bf052f3deba2
JRaams <J>
2020-05-26 13:26:01 +0200

    Add database migration + seed for Partners

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StatementSeeder::class);
         $this->call(MunicipalityStatementSeeder::class);
         $this->call(TextblocksSeeder::class);
+        $this->call(PartnersSeeder::class);
     }
 }
